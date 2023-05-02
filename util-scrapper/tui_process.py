7d969768afc7bab1fb1709272f1f0844630a4cf2
311Volt <113amper@gmail.com>
2023-05-02 10:26:43 +0200

    initial dataset

--- util-scrapper/tui_process.py
+++ util-scrapper/tui_process.py
@@ -1,4 +1,120 @@
+import datetime as dt
 import json
 import os
+import random
 
 
+def loaddata(filename):
+    with open(filename, "r") as tuidata:
+        return json.load(tuidata)["offers"]
+
+
+def save_table(obj, filename):
+    with open(filename, "w") as outfile:
+        json.dump(obj, outfile, indent=4)
+
+
+def create_hotels(offers):
+    out_hotels = dict()
+    for offer in offers:
+        if "hotelCode" in offer:
+            out_hotels[offer["hotelCode"]] = {
+                "code": offer["hotelCode"],
+                "name": offer["hotelName"],
+                "standard": offer["hotelStandard"],
+                "latitude": offer["latitude"],
+                "longitude": offer["longitude"],
+                "airportCode": offer["departureFlight"]["arrival"]["airportCode"],
+                "country": offer["breadcrumbs"][0]["label"],
+                "numSingleRooms": random.randrange(10, 30),
+                "numDoubleRooms": random.randrange(20, 40),
+                "numTripleRooms": random.randrange(10, 25),
+                "numQuadRooms": random.randrange(7, 15),
+                "ageRange0": {
+                    "upperBound": random.randrange(3, 6),
+                    "pricePerNight": random.randrange(0, 90) * offer["hotelStandard"]
+                },
+                "ageRange1": {
+                    "upperBound": random.randrange(8, 13),
+                    "pricePerNight": random.randrange(55, 130) * offer["hotelStandard"]
+                },
+                "ageRange2": {
+                    "upperBound": random.randrange(16, 19),
+                    "pricePerNight": random.randrange(35, 90) * offer["hotelStandard"]
+                }
+            }
+    return out_hotels
+
+
+def create_airports(offers):
+    out_airports = []
+    for offer in offers:
+        flight = offer["departureFlight"]
+        departure = flight["departure"]
+        arrival = flight["arrival"]
+        out_airports.append({
+            "code": departure["airportCode"],
+            "name": departure["airportName"],
+            "forDeparture": True
+        })
+        out_airports.append({
+            "code": arrival["airportCode"],
+            "name": arrival["airportName"],
+            "forDeparture": False
+        })
+    return out_airports
+
+
+def create_flights(airports, year):
+    out_flights = []
+
+    dep_airports = [airport for airport in airports if airport["forDeparture"]]
+    arr_airports = [airport for airport in airports if not airport["forDeparture"]]
+
+    start_date = dt.date(year, 1, 1).toordinal()
+    end_date = dt.date(year + 1, 1, 1).toordinal()
+
+    flight_num_counter = 1410
+
+    for date_ord in range(start_date, end_date):
+        midnight = dt.datetime.fromordinal(date_ord)
+        num_flights = random.randrange(20, 40)
+
+        for i in range(num_flights):
+            start_time = midnight + dt.timedelta(minutes=random.randrange(0, 1440))
+            end_time = start_time + dt.timedelta(minutes=random.randrange(60, 300))
+
+            out_flights.append({
+                "flightNumber": flight_num_counter,
+                "departure": {
+                    "airportCode": random.choice(dep_airports)["code"],
+                    "date": start_time.date().isoformat(),
+                    "time": start_time.time().strftime("%H:%M:%S")
+                },
+                "arrival": {
+                    "airportCode": random.choice(arr_airports)["code"],
+                    "date": end_time.date().isoformat(),
+                    "time": end_time.time().strftime("%H:%M:%S")
+                }
+            })
+
+            flight_num_counter += 1
+
+    return out_flights
+
+
+def entry():
+    initialdata = loaddata("tuidata.json")
+    hotels = create_hotels(initialdata)
+    airports = create_airports(initialdata)
+    flights = create_flights(airports, 2023)
+
+    os.makedirs("output", exist_ok=True)
+
+    save_table(hotels, "output/hotels.json")
+    save_table(airports, "output/airports.json")
+    save_table(flights, "output/flights.json")
+
+
+if __name__ == "__main__":
+    entry()
