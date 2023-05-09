c496ed3e8cc100439e45f6206bf4b2cf3ee6d7e7
311Volt <113amper@gmail.com>
2023-05-09 10:23:21 +0200

    delete legacy dirs

--- BUILD.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mvn package -Dmaven.test.skip=true
-cp ./svc-touroperator/target/rsww-touroperator-0.0.1-SNAPSHOT.jar ./docker/svc-touroperator/touroperator.jar
-cp ./svc-hotel/target/rsww-hotel-0.0.1-SNAPSHOT.jar ./docker/svc-hotel/hotel.jar
--- docker/rebuild-and-run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-(cd ..; ./BUILD.sh) && docker-compose down && docker-compose up --build
