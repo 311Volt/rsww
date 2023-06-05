53e33c5df9117412afad68475d31cbc07ca5bf2b
311Volt <113amper@gmail.com>
2023-06-05 18:36:35 +0200

    add hotel reservation command

--- lib-messages/src/main/java/com/yetistudios/rsww/messages/command/ReserveHotelCommand.java
+++ lib-messages/src/main/java/com/yetistudios/rsww/messages/command/ReserveHotelCommand.java
@@ -6,6 +6,7 @@ import lombok.Data;
 @Data
 @Builder
 public class ReserveHotelCommand {
+    public String reservationId;
     public String hotelCode;
     public long timestampBegin;
     public long timestampEnd;
---
+++ lib-messages/src/main/java/com/yetistudios/rsww/messages/event/HotelReservationSuccessfulEvent.java
@@ -0,0 +1,10 @@
+package com.yetistudios.rsww.messages.event;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class HotelReservationSuccessfulEvent {
+    public String reservationId;
+}
