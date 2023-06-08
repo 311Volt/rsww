32f549a51e2000f021c2bd1e9a5fc5cf96ecb6c9
patrykcommander <patryko@bydnet.pl>
2023-06-08 13:37:26 +0200

    Python scripts for uploading flights data to postgres DB

---
+++ python-uploadToDB/postgres/flights/postgres_config.py
@@ -0,0 +1,6 @@
+USER="postgres"
+PASSWORD="password"
+SVR_ADDRESS="127.0.0.1"
+PORT="5432"
+
+DB_URL = 'postgresql+psycopg2://{user}:{pw}@{ipv4}:{port}/flight-svc'.format(user=USER,pw=PASSWORD, ipv4=SVR_ADDRESS, port=PORT)
