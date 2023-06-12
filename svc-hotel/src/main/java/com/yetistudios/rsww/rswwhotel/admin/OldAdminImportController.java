1a79686e41778d1eee1cf2e07662977edf0cab90
311Volt <113amper@gmail.com>
2023-06-12 14:15:29 +0200

    addr change

--- svc-hotel/src/main/java/com/yetistudios/rsww/rswwhotel/admin/OldAdminImportController.java
+++ svc-hotel/src/main/java/com/yetistudios/rsww/rswwhotel/admin/OldAdminImportController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("admin")
 @Profile("!test")
-public class AdminImportController {
+public class OldAdminImportController {
 
     @Autowired
     private AdminImportService adminImportService;
